a98253011c7bfe4b74bb63ff552ca2fd8ac94028
QAQSly <91786907+QAQSly@users.noreply.github.com>
2024-12-07 20:00:09 +0800

    netty

---
+++ gaia_server/src/main/java/com/gaia/Server/NettyServer.java
@@ -0,0 +1,40 @@
+package com.gaia.Server
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class NettyServer {
+     private static final Logger logger = LogManager.getLogger(NettyServer.class);
+    public void StartServer() {
+        EventLoopGroup bossGrop = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap sb = new ServerBootstrap();
+            sb.group(bossGrop, workerGroup)
+            .channel(NioServerSocketChannel.class)
+            .childHandler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) {
+                    ch.pipeline().addLast(new BusinessHandler());
+                }
+            });
+
+            ChannelFuture cf = sb.bind(8080).sync();
+            cf.channel().closeFuture().sync();
+
+        } catch (Exception err) {
+            logger.info(err.toString());
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGrop.shutdownGracefully();
+        }
+    }
+}
