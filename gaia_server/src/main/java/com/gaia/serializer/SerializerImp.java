8203bb286190f7c132616539b33bf89ac5ea5599
QAQSly <2015672764@qq.com>
2024-12-14 12:56:19 +0800

    update

--- gaia_server/src/main/java/com/gaia/serializer/SerializerImp.java
+++ gaia_server/src/main/java/com/gaia/serializer/SerializerImp.java
@@ -1,5 +1,29 @@
 package com.gaia.serializer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class SerializerImp implements Serializer {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public byte[] serializer(Object obj) {
+        try {
+            
+            objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException("序列化失败", e);
+        }
+        return null;
+    }
+
+    @Override
+    public <T> T deserializer(byte[] bytes, Class<T> clazz) {
+        try {
+            objectMapper.readValue(bytes, clazz);
+        } catch (Exception e) {
+            throw new RuntimeException("反序列化失败", e);
+        }
+        return null;
+    }
 
 }
---
+++ gaia_server/src/main/java/com/gaia/service/ServiceRegistry.java
@@ -0,0 +1,20 @@
+package com.gaia.service;
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ServiceRegistry {
+    private final Map<String, Object> services = new HashMap<>();
+
+    public void register(String className, Object serviceImp) {
+        services.put(className, serviceImp);
+        log.info("注册服务" + className);
+    }
+
+    public void getRegister(String className) {
+        services.get(className);
+        log.info("获取服务" + className);
+    }
+}
